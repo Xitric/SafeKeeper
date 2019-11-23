626b998fd1adfd68baf020101c1cbfcd563167d6
Emil Nielsen <emilqz@gmail.com>
2019-11-23 13:25:49 +0100

    List preferences

--- privacyenforcer/src/main/java/dk/sdu/privacyenforcer/ui/SettingsFragment.java
+++ privacyenforcer/src/main/java/dk/sdu/privacyenforcer/ui/SettingsFragment.java
@@ -1,37 +1,50 @@
 package dk.sdu.privacyenforcer.ui;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import java.util.Arrays;
 
-import dk.sdu.privacyenforcer.R;
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class SettingsFragment extends Fragment {
+    private String[] permissions = new String[]{
+            Privacy.Permission.SEND_LOCATION,
+            Privacy.Permission.SEND_ACCELERATION,
+            Privacy.Permission.SEND_CONTACTS
+    };
 
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
+    private String[] mutations = new String[]{
+          "Allow", "Block", "Fake"
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+        Context context = getPreferenceManager().getContext();
+        PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(context);
+
+        for (String permission : permissions) {
+            ListPreference listPreference = new ListPreference(context);
+            listPreference.setEntries(mutations);
+            listPreference.setEntryValues(mutations);
+            listPreference.setKey(permission);
+            listPreference.setTitle(permission);
+            listPreference.setSummary("Steal your info");
+            screen.addPreference(listPreference);
+        }
+
+        setPreferenceScreen(screen);
     }
 
 }
