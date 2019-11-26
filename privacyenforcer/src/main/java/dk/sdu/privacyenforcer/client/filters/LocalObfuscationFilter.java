7629483364457ac2acb7536b222c596a7d0b01ea
Emil Nielsen <emilqz@gmail.com>
2019-11-26 10:25:33 +0100

    Fix

--- privacyenforcer/src/main/java/dk/sdu/privacyenforcer/client/filters/LocalObfuscationFilter.java
+++ privacyenforcer/src/main/java/dk/sdu/privacyenforcer/client/filters/LocalObfuscationFilter.java
@@ -1,13 +1,27 @@
 package dk.sdu.privacyenforcer.client.filters;
 
-import dk.sdu.privacyenforcer.client.RequestBody;
+
+import org.json.JSONObject;
+
+import dk.sdu.privacyenforcer.client.RequestUrl;
 import dk.sdu.privacyenforcer.client.ViolationCollection;
+import dk.sdu.privacyenforcer.ui.Privacy;
 
 public class LocalObfuscationFilter implements Filter {
 
+
     @Override
-    public void filter(RequestBody context, ViolationCollection violations) {
+    public void setMode(Privacy.Mutation mode) {
 
     }
 
+    @Override
+    public void filter(RequestUrl requestUrl, ViolationCollection violations) {
+
+    }
+
+    @Override
+    public void filter(JSONObject body, ViolationCollection violations) {
+
+    }
 }
--- privacyenforcer/src/main/java/dk/sdu/privacyenforcer/client/mutators/LocalObfuscationMutator.java
+++ privacyenforcer/src/main/java/dk/sdu/privacyenforcer/client/mutators/LocalObfuscationMutator.java
@@ -1,12 +1,19 @@
 package dk.sdu.privacyenforcer.client.mutators;
 
-import dk.sdu.privacyenforcer.client.PrivacyViolation;
-import dk.sdu.privacyenforcer.client.RequestBody;
+import org.json.JSONObject;
+
+import dk.sdu.privacyenforcer.client.RequestUrl;
 
 public class LocalObfuscationMutator implements DataMutator{
 
+
     @Override
-    public void mutate(RequestBody context, PrivacyViolation violation) {
+    public void mutate(RequestUrl requestUrl, String[] flaggedParameters) {
+
+    }
+
+    @Override
+    public void mutate(JSONObject[] flaggedObjects) {
 
     }
 }
