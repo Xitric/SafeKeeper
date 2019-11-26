5fa3961d0b34264975ee16288db58658e703c642
Emil Nielsen <emilqz@gmail.com>
2019-11-26 11:15:27 +0100

    Room

---
+++ privacyenforcer/src/main/java/dk/sdu/privacyenforcer/client/repository/MutatorEntity.java
@@ -0,0 +1,15 @@
+package dk.sdu.privacyenforcer.client.repository;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class MutatorEntity {
+
+    @PrimaryKey
+    public int mid;
+
+    
+
+}
